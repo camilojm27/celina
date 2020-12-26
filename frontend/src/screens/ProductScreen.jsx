8c9555ac4ed851ea6c49adb15ab225233a5ae95a
Camilo José Mezú Mina <camilo272001@gmail.com>
2020-12-26 06:50:03 -0500

    📱ProductScreen Skeleton Working on mobile.

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -5,27 +5,64 @@ import SimpleImageSlider from "react-simple-image-slider";
 import stilos from './styles/ProductScreen.css'
 
 const ProductScreen = (props) => {
+    const size = sliderSize();
     const product = data.products.find(x => x._id === Number(props.match.params.id))
     return(
         <section className="details">
-            <div className="details__product">
-            <h1>{product.name}</h1>
+            <figure className="details__product">
+            <h4>{product.name}</h4>
                 <SimpleImageSlider
-                width={800}
-                height={500}
+                width={size}
+                height={size}
                 images={product.images.map(img => ({url: img} ))}
                 style={stilos}
                 />
-                <h2>{product.price}</h2>
 
-
-            </div>
+            </figure>
             <div className="details__buy">
+                <h2>{`$ ${product.price}`}</h2>
+                    <select id="size" name="select">
+                        <option value="value1" defaultValue>Talla M</option>
+                        <option value="value2" >Talla S</option>
+                        <option value="value3">Talla XL</option>
+                    </select>
+
+
+                <select id="quantity" name="quantity">
+                    <option value="value1" defaultValue>1</option>
+                    <option value="value2" >2</option>
+                    <option value="value3">3</option>
+                </select>
+                <button id="cart">
+                    Añadir al carrito
+                </button>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium alias architecto blanditiis, corporis debitis deleniti dignissimos dolore, error ex illo, inventore ipsam laboriosam maxime mollitia nemo neque nisi odio optio praesentium sint soluta suscipit temporibus totam. Iste sunt tempora tenetur unde. Assumenda consequuntur distinctio enim eos error est impedit, iste laborum libero magnam obcaecati reiciendis sapiente sunt veritatis?</p>
 
             </div>
         </section>
     )
 }
+
+function sliderSize (){
+    let size, width = window.screen.availWidth;
+    if (width <= 500){
+        size = 300;
+    }
+
+    else if (width <= 720){
+        size = 400;
+    }
+    else{
+        size = 500;
+    }
+
+    return size;
+
+}
+
+// alert(window.screen.availHeight);
+// alert(window.screen.availWidth);
+
 
 export default ProductScreen
 
